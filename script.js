4559d345e2143c3ef5b39087b43e71d94bbd0b31
Nezerwa <88612346+Nezerwa@users.noreply.github.com>
2022-11-25 11:29:07 +0200

    Remove unused code

--- script.js
+++ script.js
@@ -17,7 +17,7 @@ const email = document.querySelector('#useremail');
 const validationAlert = document.querySelector('.alert');
 const contactForm = document.querySelector('.contact-me');
 contactForm.addEventListener('submit', (event) => {
-  // Write your code below this line.
+
   const regex = /[A-Z]/;
   const emailContent = email.value;
   if (regex.test(emailContent)) {
